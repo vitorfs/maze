a9fd3727a9c43a3c4d6b0a286eece043ed717016
Vitor Freitas <vitorfs@gmail.com>
2013-06-04 09:48:42 -0300

    Kick-off to for depth-first search

---
+++ maze.c
@@ -0,0 +1,25 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  maze.c
+ *
+ *    Description:  Implementation of a depth-first search for solving maze
+ *                  problems
+ *
+ *        Version:  1.0
+ *        Created:  04/06/2013 09:45:55
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Vitor Freitas (vfs), vitorfs@gmail.com
+ *        Company:  Universidade Federal de Juiz de Fora (UFJF)
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]) {
+  return 0;
+}
